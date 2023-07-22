18b5c56ec83e00c8d71915253d0554b3ba2ae97b
PRAVINKUMAR CHANDRAKANT MAHOOR <108792284+CodeWithPM@users.noreply.github.com>
2023-07-22 13:58:19 +0530

    Add files via upload

---
+++ practice2.java
@@ -0,0 +1,13 @@
+public class practice2 {
+    public static void main(String []args){
+        int n=10;
+        int m=4;
+        for(int i=1;i<=m;i++){
+
+            for (int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+        System.out.println("");
+        }
+    }
+}
---
+++ practice4.java
@@ -0,0 +1,14 @@
+public class practice4 {
+        public static void main(String[]args){
+            int n = 1;
+            int m=4;
+            for(int i=4;i>=n;i--){
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+
+                }
+                System.out.println("");
+
+            }
+        }
+}
